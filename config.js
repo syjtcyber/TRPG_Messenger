7f847caac8ed8dd731f3622d3bec5f736884b46e
syjtcyber <dmae36569@gmail.com>
2026-08-12 10:35:37 +0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// GitHub Pages에 올리기 전에 이 파일의 두 값만 수정하세요.
-// Supabase > Project Settings > API 에서 확인할 수 있습니다.
-window.TRPG_CONFIG = {
-  SUPABASE_URL: "여기에_https://ebkaybwxonlojnkubvrm.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_A-PsGSejOL4TA7BrP1b_Kw_d8uHs2KU"
-};
